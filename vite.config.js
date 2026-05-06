1ccae6c16bf0fdd9c55dc8c800ef80cf3b47865d
InMochi <ooer379@gmail.com>
2026-05-06 18:44:00 +0700

    Sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: 
   [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://tfarrel-dev.vercel.app/',
+    })
 
   ],
 })
